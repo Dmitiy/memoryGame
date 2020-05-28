b08d635d6a3a742ec3fd34b4562e1a19358ffdc3
Dmitiy <dmitriy.dreamcast@gmail.com>
2020-05-28 13:44:36 +0300

    Updates

---
+++ precache-manifest.1183f2d20e39cdba9c7b860e22327ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b39b10433adc22a6c24e6e80efe796",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53bf275626cc691360f5",
+    "url": "/static/css/main.44b5d84a.chunk.css"
+  },
+  {
+    "revision": "2760ae84af89c11671eb",
+    "url": "/static/js/2.d7749c73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7749c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53bf275626cc691360f5",
+    "url": "/static/js/main.abccd70f.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  }
+]);
